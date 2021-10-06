26aa61811a23ccf82dcfe602b134d3d638c2f39b
Yasuhiro Iuchi <yasu0414atc@gmail.com>
2021-10-06 13:56:58 +0900

    add timeline sql

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request 
 from flask_ngrok import run_with_ngrok
+import sqlite3
 
 app = Flask(__name__)
 run_with_ngrok(app)
@@ -7,7 +8,16 @@ run_with_ngrok(app)
 @app.route('/')
 def hello():
   #return 'Hello World!'
-  return render_template('index.html')
+  #data=zip(tweet_list,date_list)
+  con = sqlite3.connect('data.db') 
+  cur = con.cursor()                                                                                                                      
+  cur.execute('SELECT * FROM tweet_table')
+  out = cur.fetchall()
+
+  con.commit()                                                                                                                               
+  con.close()
+
+  return render_template('index.html',data=out)
 
 if __name__ == '__main__':
   app.run()
